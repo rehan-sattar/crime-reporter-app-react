93777d5c4cfbdc3258486ebd013c607e42205798
Rehan-Sattar <rehansattar117@gmail.com>
2018-06-06 01:38:09 +0500

    missing person file

--- src/components/reports/missing-person.js
+++ src/components/reports/missing-person.js
@@ -1,31 +1,52 @@
 import React, { Component } from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
-const MissingPerson = () => (
-  <div>
-    <div className="container">
-      <div className="row mt-3">
-        <div className="card w-100">
-          <div className="card-body">
-            <h5 className="card-title">Name : Rehan <br /> Report Title: Missing Person <br /> Missing Date: 20-2-1999  </h5>
-            <hr />
-            <h5> Description:  </h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardstent.</p>
+
+class MissingPerson extends Component {
+  render() {
+    return (
+      <div>
+        {console.log("inside Missing Person component: ", this.props.missingPersonData)}
+        <div className="container">
+          <div className="row mt-3">
+            <div className="card w-100">
+              <div className="card-body">
+                <h5 className="card-title">Name : Rehan <br /> Report Title: Missing Person <br /> Missing Date: 20-2-1999  </h5>
+                <hr />
+                <h5> Description:  </h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardstent.</p>
+              </div>
+            </div>
           </div>
+
+          {this.props.missingPersonData.map((item, index) => (
+            <div className="row mt-3">
+              <div className="card w-100">
+                <div className="card-body">
+                  <h5 className="card-title">Name : {item.userName} <br />
+                    Missing person: {item.missingPersonName} <br />
+                    Report Title: {item.titleOfCompliaint} <br />
+                    Missing Date: {item.dateOfMissing} 
+                  </h5>
+                  <hr />
+                  <h5> Description:  </h5>
+                  <p className="card-text">{item.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+
         </div>
       </div>
-      <div className="row mt-3">
-        <div className="card w-100">
-          <div className="card-body">
-            <h5 className="card-title">Name : Soha <br /> Report Title: Missing Person  <br /> Missing Date: 18-8-1997   </h5>
-            <hr />
-            <h5> Description:  </h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cardstent.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
-export default MissingPerson;
+
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    missingPersonData: state.report.missingPerson
+  }
+}
+export default connect(mapStateToProps)(MissingPerson);
