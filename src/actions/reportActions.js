e5dd8ecd89140c254b2f130bc1e10bed8fa42e20
Rehan-Sattar <rehansattar117@gmail.com>
2018-06-06 01:35:01 +0500

    Actions of Reports

--- src/actions/reportActions.js
+++ src/actions/reportActions.js
@@ -1,28 +1,39 @@
-import { fire as firebase } from "../components/firebase/firebase";
+import {
+  fire as firebase
+} from "../components/firebase/firebase";
 export function handleCrimeReport(detials) {
-  return () => {
-    firebase.database().ref().child("/crime").push(detials)
-      .then(response => console.log('perfectly written'))
+  return (dispatch) => {
+    firebase.database().ref("reports").child("crime").push(detials)
+      .then(() => dispatch({
+        type: "CRIME_REPORT",
+        payload: {...detials}
+      }))
       .catch(error => console.log('error: ', error));
   }
 
 };
 
-
 export function handeComplaints(detials) {
-  return () => {
-    firebase.database().ref().child("/complaints").push(detials)
-      .then(response => console.log('perfectly written'))
+  return (dispatch) => {
+    firebase.database().ref("reports").child("complaints").push(detials)
+      .then(() => dispatch({
+        type: "COMPLAINT_REPORT",
+        payload: {...detials}
+      }))
       .catch(error => console.log('error: ', error));
   }
   
+  
+
 };
 
 export function handleMissingPerson(detials) {
-  return () => {
-    firebase.database().ref().child("/missing-person").push(detials)
-      .then(response => console.log('perfectly written'))
+  return (dispatch) => {
+    firebase.database().ref("reports").child('missing-person').push(detials)
+      .then(() => dispatch({
+        type: "MISSING_PERSON",
+        payload: {...detials}
+      }))
       .catch(error => console.log('error: ', error));
   }
-
 };
