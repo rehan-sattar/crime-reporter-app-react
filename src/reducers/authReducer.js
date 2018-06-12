85653be8960ff36b2750ac42ccf6b76fef95fcb3
Rehan-Sattar <rehansattar117@gmail.com>
2018-06-12 17:00:09 +0500

    Store auth Handler

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,13 @@
+export default (state = {}, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      console.log('User Logged in| Token: ', action.uid);
+      return {
+        uid: action.uid
+      }
+    case 'LOGOUT':
+      return {};
+    default:
+      return state;
+  };
+};
