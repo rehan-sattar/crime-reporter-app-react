a2afef7ec67f09ee7a292f2fad63f2addb0d4a96
Rehan-Sattar <rehansattar117@gmail.com>
2018-07-13 06:53:17 -0700

    root file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./store";
-import  { startSetReport } from "./actions/reportActions";
+import { startSetReport } from "./actions/reportActions";
 import "./components/firebase/firebase";
 store.dispatch(startSetReport());
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
