4c6436d0cfab0614af82fd5551ecaa3cf7255f29
Rehan-Sattar <rehansattar117@gmail.com>
2018-07-13 06:50:56 -0700

    Authentication for user

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,31 @@
 import { fire as firebase, googleAuthProvider } from "../components/firebase/firebase";
 export const loginViaGoogle = () => {
+  console.log('Login Via Google');
   return () => {
     return firebase.auth().signInWithPopup(googleAuthProvider);
   };
+};
+
+
+export const startLogin = (state) => {
+  const { email, password } = state;
+  console.log(email, password);
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log("User Response: ", response.user);
+        dispatch({
+          type: "LOGIN_WITH_EMAIL_PASS",
+          payload: response.user.uid
+        })
+      }).catch(error => {
+        dispatch({
+          type: "Error",
+          payload: error.message
+        })
+      });
+  }
+
 };
 
 
