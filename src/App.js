f1d910100316e067663a7bfb88c2dc89dc190afb
Rehan-Sattar <rehansattar117@gmail.com>
2019-09-03 17:23:14 +0500

    [update] - refactor into funcitonal component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,5 @@
-import React, { Component } from 'react';
-import LoginUser from './components/Forms/Login';
-import SignInUser from './components/Forms/SignIn';
-import Reports from './components/reports/reports';
-import UserDashboard from './components/userDashobard/main';
-import { fire as firebase } from './components/firebase/firebase';
-import { Router, Route, Switch, Link } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
-import Header from './components/Header/Header';
-import AdminDashboard from './AdminDashboard/AdminDashboard';
-// import Admin_login from './AdminDashboard/Admin_login';
-export const history = createHistory();
-let status = undefined;
-class App extends Component {
-  state = {
-    isAuthenticated: false
-  };
-  componentDidMount() {
-    const token = localStorage.getItem('token');
-    if (token) {
-      this.setState({
-        isAuthenticated: true
-      });
-    } else {
-      this.setState({ isAuthenticated: false });
-    }
-  }
+import React, { Component } from "react";
+import AppRoutes from "./routes/routes";
 
-  render() {
-    return (
-      <div>
-        <Router history={history}>
-          <div>
-            <Header authenticated={this.state.isAuthenticated} />
-            <Switch>
-              <Route exact path='/' component={LoginUser} />
-              <Route path='/sign-in' component={SignInUser} />
-              <Route path='/admin-dashboard' component={AdminDashboard} />
-              {/* <Route path='/admin-login' component={Admin_login} /> */}
-              <Route path='/reports' component={Reports} />
-              <Route path='/user-dashboard' component={UserDashboard} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}
-
+const App = () => <AppRoutes />;
 export default App;
