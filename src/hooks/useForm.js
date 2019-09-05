96c8a81ffdf679bec459a1967995ec872a7f12c1
Rehan-Sattar <rehansattar117@gmail.com>
2019-09-05 14:13:24 +0500

    [feat]-add handleSubmit & values pattern to useForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,9 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const useForm = initValue => {
-  const [value, setState] = useState(initValue);
-  const setValue = value => setState(value);
-  return [value, setValue];
+const useForm = (callback, defaultValues) => {
+  const [values, setValues] = useState({ ...defaultValues });
+  const handleSubmit = event => {
+    if (event) event.preventDefault();
+    callback();
+  };
+  const handleChange = event => {
+    event.persist();
+    setValues(values => ({
+      ...values,
+      [event.target.name]: event.target.value
+    }));
+  };
+  return {
+    handleChange,
+    handleSubmit,
+    values
+  };
 };
 
 export default useForm;
